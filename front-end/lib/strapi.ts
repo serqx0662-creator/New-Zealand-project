79b67e6e6ac6262ab5c1e14701c639acf061c78a
serqx0662-creator <serqx0662@gmail.com>
2026-06-01 20:11:57 +0600

    fix

--- front-end/lib/strapi.ts
+++ front-end/lib/strapi.ts
@@ -1,13 +1,9 @@
 export async function getPrograms(lang: string = 'ru') {
-    const baseUrl = "http://127.0.0.1:1337";
+    const baseUrl = process.env.NEXT_PUBLIC_STRAPI_URL || "http://127.0.0.1:1337";
 
-    // Добавляем параметр locale в запрос
-    // Strapi i18n использует формат ?locale=ru или ?locale=en
     const url = `${baseUrl}/api/programs?locale=${lang}&populate=*`;
 
     const res = await fetch(url, {
-        // revalidate: 60 хорош для продакшена,
-        // но при разработке i18n можно поставить 0, чтобы сразу видеть изменения
         next: { revalidate: 60 }
     });
 
